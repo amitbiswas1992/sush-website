c0b4d79c9393ff1d6644a3c745c6d94f518f75ba
Amit Biswas <biswas1992@hotmail.com>
2018-06-12 10:45:08 -0400

    Update content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,12 +45,13 @@
                     <h1>Hi, I'm Sushmita !</h1>
 
                     <p class="lead">Bachelor Of Business Administration </p>
+                    <p class="lead">Notre Dame University Bangladesh </p>
+
                     <ul class="social-icon">
                         <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                         <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                         <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-slack" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+
                     </ul>
                 </div>
                 <div class="profile-img"></div>
